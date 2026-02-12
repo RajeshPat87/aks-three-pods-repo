d88db21373a86f8e8a64eb6797d899d4c34c45d0
e34887 <e34887@365oft.top>
2026-02-12 14:52:27 +0000

    Updated acr.tf

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.aks.name
   location            = azurerm_resource_group.aks.location
   sku                 = var.acr_sku
-  admin_enabled       = false
+  admin_enabled       = true
 
   tags = local.common_tags
 
